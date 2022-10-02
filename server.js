85887fa350a91396ce13239727a69a6f0b54652a
krzysztofMlczk <krzysztof.malczak@ardentcode.com>
2022-10-02 17:23:04 +0200

    ehh

--- server.js
+++ server.js
@@ -78,7 +78,11 @@ app.post("/notify-me", async (request, response) => {
 app.post("/notify-all", async (request, response) => {
   console.log("Notifying all subscribers");
   const subs = await subscriptions.list();
-
+  console.log(
+    subs.results.map((sub) => {
+      endpoint: sub.key;
+    })
+  );
   if (subs.results.length > 0) {
     sendNotifications(
       subs.results.map((sub) => {
